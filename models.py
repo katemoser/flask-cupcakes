1f3655283ebec9795e1749be38b3311e4e0ddb6a
Kate Moser <komoser@gmail.com>
2022-02-07 14:21:36 -0600

    Made cupcake model

--- models.py
+++ models.py
@@ -1,1 +1,15 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Cupcake(db.Model):
+    """Cupcake"""
+
+    __tablename__ = "cupcakes"
+    
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    flavor = db.Column(db.Text, nullable = False)
+    size = db.Column(db.Text, nullable = False)
+    rating = db.Column(db.Integer, nullable = False)
+    image = db.Column(db.Text, nullable = False, default="https://tinyurl.com/demo-cupcake")
