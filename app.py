961811edcae1985f938882c268aded4204cbeb31
Kate Moser <komoser@gmail.com>
2022-02-07 16:22:56 -0600

    Wrote Post request for new cupcake

--- app.py
+++ app.py
@@ -13,32 +13,40 @@ connect_db(app)
 db.create_all()
 
 
-@app.route("/api/cupcakes", methods=['GET','POST'])
+@app.route("/api/cupcakes", methods=['GET'])
 def display_cupakes():
     """ Get data about all cupcakes"""
 
     cupcakes = Cupcake.query.all()
     serialized = [cupcake.serialize() for cupcake in cupcakes]
 
+    return jsonify(cupcakes=serialized)
 
 
-    # we will have an if else I'm assuming
-
-    new_cupcake = Cupcake(
-        id = request.json.id,
-        flavor = request.json.flavor,
-        size = request.json.size,
-        rating = request.json.rating,
-        image = request.json.image,
-    )
-
-    return jsonify(cupcakes = serialized)
-
-
-@app.route("/api/cupcakes/<cupcake-id>", methods=['GET'])
+@app.route("/api/cupcakes/<int:cupcake_id>", methods=['GET'])
 def display_cupake(cupcake_id):
     """ Get data about a single cupcake."""
 
     cupcake = Cupcake.query.get_or_404(cupcake_id)
 
-    return jsonify(cupcake = cupcake)
+    return jsonify(cupcake=cupcake)
+
+
+@app.route("api/cupcakes", methods=["POST"])
+def make_new_cupcake():
+    """Make a new cupcake"""
+
+    new_cupcake = Cupcake(
+        id=request.json.id,
+        flavor=request.json.flavor,
+        size=request.json.size,
+        rating=request.json.rating,
+        image=request.json.image,
+    )
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized_cupcake = new_cupcake.serialize()
+
+    return (jsonify(cupcake=serialized_cupcake), 201)
